e32867f8305875a5b3371793c55f87a4cec8a0db
shiladityab24 <shiladityab24@yahoo.com>
2023-09-22 04:01:58 +0530

    imported static resources in the js file

--- force-app/main/default/lwc/weatherApp/weatherApp.js
+++ force-app/main/default/lwc/weatherApp/weatherApp.js
@@ -1,6 +1,22 @@
 import { LightningElement } from 'lwc';
 const API_KEY = 'fd386428ac134167f8cdd28236c54310'
+import WEATHER_ICON from '@salesforce/resourceUrl/weatherAppIcons'
 export default class WeatherApp extends LightningElement {
+  clearIcon = WEATHER_ICON + '/weatherAppIcons/clear.svg'
+  cloudIcon = WEATHER_ICON + '/weatherAppIcons/cloud.svg'
+  dropletIcon = WEATHER_ICON + '/weatherAppIcons/droplet.svg'
+  hazeIcon = WEATHER_ICON + '/weatherAppIcons/haze.svg'
+  mapIcon = WEATHER_ICON + '/weatherAppIcons/map.svg'
+  rainIcon = WEATHER_ICON + '/weatherAppIcons/rain.svg'
+  snowIcon = WEATHER_ICON + '/weatherAppIcons/snow.svg'
+  stormIcon = WEATHER_ICON + '/weatherAppIcons/storm.svg'
+  thermometerIcon = WEATHER_ICON + '/weatherAppIcons/thermometer.svg'
+  arrowBackIcon = WEATHER_ICON + '/weatherAppIcons/arrow-back.svg'
+  
+
+
+
+
   cityName = ''
   loadingText = ''
   isError = false
